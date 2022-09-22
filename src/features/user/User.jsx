462f9a0267b1a6ed58a9f518cb6ab81cf18909c0
Shirish Pokhrel <shirish.pokhrel@appsinvo.com>
2022-09-22 13:46:31 +0530

    changes

--- src/features/user/User.jsx
+++ src/features/user/User.jsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Table, Typography } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { FetchRepoForUser } from "./userSlice";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
 function User() {
   const dispatch = useDispatch();
@@ -9,14 +16,14 @@ function User() {
   const [dataSource, setDataSource] = useState([]);
   const data = useSelector((store) => store.user.data);
 
-  useEffect(() => {
+  const handleSearch = () => {
     if (username) {
       dispatch(FetchRepoForUser(username));
     }
-  }, [dispatch, username]);
+  };
 
   return (
-    <div className="m-auto ">
+    <div className="p-4 mb-2">
       <div className="d-flex">
         <label className="fw-bold m-2">Username</label>
         <input
@@ -27,9 +34,19 @@ function User() {
             setUsername(e.target.value);
           }}
         />
+
+        <Button
+          variant="contained"
+          onClick={() => {
+            handleSearch();
+          }}
+          className="mx-2"
+        >
+          Submit
+        </Button>
       </div>
 
-      <table className="table-responsive m-auto">
+      {/* <table className="table-responsive m-auto">
         <thead>
           <tr>
             <th scope="col mx-2">Public Repos</th>
@@ -46,7 +63,36 @@ function User() {
             );
           })}
         </tbody>
-      </table>
+      </table> */}
+
+      {data?.length > 0 && (
+        <TableContainer component={Paper} className="mt-4">
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Repo Name</TableCell>
+                <TableCell align="right">Star Count</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data?.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">
+                    {row.watchers_count}{" "}
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </div>
   );
 }
